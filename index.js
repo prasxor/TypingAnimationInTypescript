62b5764796ef5ad5c71e34e59726c62687b9a8b4
Prashant kumar sinha <prasxor@gmail.com>
2024-08-30 20:59:52 +0530

    done

---
+++ index.js
@@ -0,0 +1,28 @@
+let typed = new Typed(".auto-type", {
+  strings: ["Coder", "Programmer", "Web designer", "Video Editor"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true,
+  fadeout: true,
+  fadeOutDelay: 500,
+});
+
+let typed2 = new Typed(".auto-typed", {
+  strings: [
+    "You are an ambitious and skilled individual ",
+    "with a strong foundation in frontend development,",
+    "including expertise in HTML, CSS, JavaScript, and React.",
+    "You've successfully created multiple websites,",
+    "such as 'Acme Degree College' and 'Gravity,'",
+    "showcasing your ability to design responsive layouts and integrate modern features.",
+    "Your problem-solving skills, combined with experience in teamwork and leadership,",
+    "evident in your projects and past work at Tech Mahindra.",
+    "Additionally, your creative side shines through your YouTube channel,",
+    "where you share music shorts and lyrics videos, engaging a growing audience.",
+  ],
+  typeSpeed: 40,
+  backSpeed: 50,
+  loop: true,
+  fadeout: true,
+  fadeOutDelay: 500,
+});
